8586735fa1aaa46d1ac5f215f7cd5978197e6ea9
fitolit-work <96796152+fitolit-work@users.noreply.github.com>
2022-06-29 09:19:05 +0300

    создание любого кол-ва звездочек

--- part3/trening-app-p3/src/components/SatarRating.jsx
+++ part3/trening-app-p3/src/components/SatarRating.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-    return [
-        <FaStar color="red"/>,
-        <FaStar color="red"/>,
-        <FaStar color="red"/>,
-        <FaStar color="grey"/>,
-        <FaStar color="grey"/>,
-    ];
+const createArray = length => [...Array(length)]; // создает массив любой заданной длины
+
+const Star = ({ selected = false }) => {// фигурные скобки нужны, потому что StarRating это компонент, компонент принимает только пропс, пропс это объект, а значит нужна деструктуризация в фигурных скобках
+    return (
+        <FaStar color={selected ? 'red' : 'grey'} />
+    )
+};
+
+export default function StarRating({count = 15}) { // фигурные скобки нужны, потому что StarRating это компонент, компонент принимает только пропс, пропс это объект, а значит нужна деструктуризация в фигурных скобках
+    return (
+        createArray(count).map((item, i) => <Star key={i}/>)
+    )
 }
